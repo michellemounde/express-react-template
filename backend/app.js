1ec7e663da4a8bb0dd045d0312cd2482a5a642da
Michelle Mounde <mitchmounde@gmail.com>
2023-04-13 11:25:02 +0300

    feat: add api router

--- backend/app.js
+++ backend/app.js
@@ -4,37 +4,10 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const helmet = require('helmet');
+const { doubleCsrf } = require('csrf-csrf');
 
 const { environment } = require('./config');
 const isProduction = environment ==='production';
-
-const { doubleCsrf } = require('csrf-csrf');
-const {
-  invalidCsrfTokenError, // This is just for convenience if you plan on making your own middleware.
-  generateToken, // Use this in your routes to provide a CSRF hash cookie and token.
-  validateRequest, // Also a convenience if you plan on making your own middleware.
-  doubleCsrfProtection, // This is the default CSRF protection middleware.
-} = doubleCsrf({ // The only required option is getSecret, the rest have sensible defaults (shown below) other than cookieOptions edited as per AAO
-  getSecret: (req) => req.secret,
-  cookieName: '__Host.test-x-csrf-token', // The name of the cookie to be used, recommend using Host prefix.
-  cookieOptions: {
-    httpOnly: true,
-    sameSite: isProduction && 'lax', // Recommend you make this strict if possible
-    secure: isProduction,
-    signed: isProduction
-  },
-  size: 64, // The size of the generated tokens in bits
-  ignoredMethods: ['GET', 'HEAD', 'OPTIONS'], // A list of request methods that will not be protected.
-  getTokenFromRequest: (req) => { // A function that returns the token from the request
-    // If the incoming request is a multipart content type
-    // then get the token from the body.
-    if (req.is('multipart')) {
-      return req.body['CSRFToken']
-    }
-    // Otherwise use the header for all other request types
-    return req.headers['x-csrf-token']
-  }
-});
 
 const routes = require('./routes');
 
@@ -42,8 +15,26 @@ const app = express();
 
 // Middleware
 app.use(morgan('dev'));
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
+
+/* This is the default CSRF protection middleware. */
+const { doubleCsrfProtection, generateToken } =
+  doubleCsrf({
+    // The only required option is getSecret, the rest have sensible defaults (shown below) other than cookieOptions edited as per AAO
+    getSecret: (req) => req.secret,
+    cookieName: isProduction && '__Host-test.x-csrf-token' || 'Test.x-csrf-token', // The name of the cookie to be used, recommend using `__Host-` prefix.
+    cookieOptions: {
+      httpOnly: true,
+      sameSite: isProduction && 'Lax', // Recommend you make this strict if possible
+      secure: isProduction,
+      path: "/"
+    },
+    size: 64, // The size of the generated tokens in bits
+    ignoredMethods: ['GET', 'HEAD', 'OPTIONS'],
+    getTokenFromRequest: (req) => req.headers["x-csrf-token"] // A function that returns the token from the request
+  });
+
 
 // Security middleware
 if (!isProduction) {
@@ -52,15 +43,18 @@ if (!isProduction) {
 }
 
   // helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet({
-    crossOriginResourcePolicy: {
-      policy: !isProduction && 'cross-origin'
-    }
-  })
-);
+if (!isProduction) {
+  app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: 'cross-origin'
+    })
+  );
+} else {
+  app.use(helmet())
+}
 
-  // Set the _csrf token and create req.csrfToken method
+
+  // Set the _csrf token and create req.csrfToken method;
 app.use(doubleCsrfProtection);
 
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 // Add a XSRF-TOKEN cookie
 router.get('/api/csrf/restore', (req, res) => {
-  const csrfToken = req.csrfToken(true);
-  res.status(200).json({ csrfToken });
+  return res.json({ csrfToken: req.csrfToken() });
 });
 
 module.exports = router;
