44c92ac8e0b6017891f2ca09351c271425f24aa5
Michelle Mounde <mitchmounde@gmail.com>
2023-04-26 16:52:59 +0300

    fix: modify redux-logger import to import logger function

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -12,7 +12,7 @@ const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => {
     if (process.env.NODE_ENV !== 'production') {
-      const logger = require('redux-logger');
+      const logger = require('redux-logger').default;
       return getDefaultMiddleware().concat(logger)
     }
   },
