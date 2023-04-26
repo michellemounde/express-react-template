f36b189053d7aca2bdb5c1b5f7c46571bb969094
Michelle Mounde <mitchmounde@gmail.com>
2023-04-26 15:25:20 +0300

    feat: create modal template

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,10 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
+import { ModalProvider, Modal } from './context/Modal';
 
 import store from './store';
 import { restoreCsrf, csrfFetch } from './store/csrf';
-
 import * as sessionActions from './store/session';
 
 if (process.env.NODE_ENV !== 'production') {
@@ -22,11 +22,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
